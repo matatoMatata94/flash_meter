40e520e992d5ef40840cfaac6c1c35eb63d6884c
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2024-02-24 12:01:49 +0100

    Finished Color Constants

--- lib/view/color/color_constants.dart
+++ lib/view/color/color_constants.dart
@@ -1,8 +1,6 @@
 import 'package:flash_meter/controller/flash_meter_theme_controller.dart';
 import 'package:flutter/material.dart';
 
-//#1668b4
-
 class ColorConstants {
   final ThemeController themeController;
 
@@ -12,14 +10,17 @@ class ColorConstants {
       ? [const Color(0xFF1668b4), const Color(0xFF144778)]
       : [Colors.blue.shade500, Colors.white];
 
-  // static List<Color> darkScaffoldBackgroundColor = [
-  //   const Color(0xFF1668b4),
-  //   const Color(0xFF144778),
-  // ];
-  // static List<Color> lightScaffoldBackgroundColor = [
-  //   Colors.blue.shade500,
-  //   Colors.white,
-  // ];
+  late Color inputBoxColor =
+      themeController.isLightMode ? Colors.white : Colors.black;
+
+  late Color deleteButtonColor =
+      themeController.isLightMode ? Colors.white : Colors.black;
+
+  late Color numberButtonColor =
+      themeController.isLightMode ? Colors.black12 : const Color(0xFF2484d3);
+
+  late Color numberButtonTextColor =
+      themeController.isLightMode ? Colors.white : const Color(0xFF0f2742);
 
   late Color appBarBackgroundColor =
       themeController.isLightMode ? Colors.blue : const Color(0xFF1668b4);
@@ -27,20 +28,9 @@ class ColorConstants {
   late Color bottomNavigationBackgroundColor =
       themeController.isLightMode ? Colors.blue : const Color(0xFF0f2742);
 
-  static Color bottomNavigationSelectedItemColor = Colors.white;
-  static Color bottomNavigationUnselectedItemColor = Colors.grey;
-
   static Color customControlButtonForegroundColor = Colors.white;
   static Color customControlButtonBackgroundColor = Colors.blue;
 
-  static Color lightInputBoxColor = Colors.white;
-  static Color darkInputBoxColor = Colors.black;
-
-  static Color lightDeleteButtonColor = Colors.white;
-  static Color darkDeleteButtonColor = Colors.black;
-
-  static Color lightNumberButtonColor = Colors.black12;
-  static Color darkNumberButtonColor = const Color(0xFF2484d3);
-  static Color lightNumberButtonTextColor = Colors.white;
-  static Color darkNumberButtonTextColor = const Color(0xFF0f2742);
+  static Color bottomNavigationSelectedItemColor = Colors.white;
+  static Color bottomNavigationUnselectedItemColor = Colors.grey;
 }
