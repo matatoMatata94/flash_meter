2002ea3e404e73932bbf1ee283a1519d99cabae7
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2024-03-05 22:44:27 +0100

    Starting to try something with the theme

---
+++ lib/view/theme/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class AppTheme {
+  ThemeData get themeData {
+    return ThemeData(
+      scaffoldBackgroundColor: ,
+      primaryColor: ,
+      dividerColor: ,
+    );
+  }
+}
